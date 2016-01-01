22bb4b167da9aa5b07a75c4f48d53bcfea745852
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:27:15 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -21,46 +21,38 @@ function getStdout(fn) {
 
 test('show', t => {
 	t.is(getStdout(cliCursor.show), SHOW);
-	t.end();
 });
 
 test('hide', t => {
 	t.is(getStdout(cliCursor.hide), HIDE);
-	t.end();
 });
 
 test('toggle', t => {
 	cliCursor.hide();
 	t.is(getStdout(cliCursor.toggle), SHOW);
-	t.end();
 });
 
 test('toggle 2', t => {
 	cliCursor.show();
 	t.is(getStdout(cliCursor.toggle), HIDE);
-	t.end();
 });
 
 test('toggle force', t => {
 	cliCursor.show();
 	t.is(getStdout(cliCursor.toggle.bind(null, true)), SHOW);
-	t.end();
 });
 
 test('toggle force 2', t => {
 	cliCursor.hide();
 	t.is(getStdout(cliCursor.toggle.bind(null, true)), SHOW);
-	t.end();
 });
 
 test('toggle force 3', t => {
 	cliCursor.show();
 	t.is(getStdout(cliCursor.toggle.bind(null, false)), HIDE);
-	t.end();
 });
 
 // used to fail, see sindresorhus/log-update#2
 test('require', t => {
 	t.is(childProcess.execSync('node index.js', {encoding: 'utf8'}), '');
-	t.end();
 });
